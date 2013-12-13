f16e9ab9358e57e1712bc8b0dfe673f989f8ab83
Patrick Wendell <pwendell@gmail.com>
2013-12-13 14:49:56 -0800

    Initial script for creating shaded verison of protobuf packages

---
+++ shaded-protobuf/create_shaded_protobuf.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+SCRATCH_DIR=/tmp/proto-scratch # Should exist and be empty
+NEW_PACKAGE_NAME=com.google.protobuf_spark
+NEW_PACKAGE_PATH=com/google/protobuf_spark
+
+set -e
+pushd $SCRATCH_DIR
+
+# Download and shade protobuf
+wget http://protobuf.googlecode.com/files/protobuf-2.4.1.zip
+unzip proto*zip
+cd protobuf-2.4.1/
+find . -type f | grep -e pom.xml$ |  xargs -I {} sed -i "s/<groupId>com.google.protobuf<\/groupId>/<groupId>org.spark-project.protobuf<\/groupId>/" {}
+find . -type f | grep -e \.xml$ -e \.java$ -e \.cc$ -e \.h$ -e \.proto$ | grep -v descriptor.pb.cc  |  xargs -I {} sed -i s/com.google.protobuf/$NEW_PACKAGE_NAME/ {}
+mkdir -p java/src/main/java/$NEW_PACKAGE_PATH
+mv java/src/main/java/com/google/protobuf/* java/src/main/java/$NEW_PACKAGE_PATH
+rm -r java/src/test
+
+# Build C++
+./configure
+make
+
+# Build Java
+pushd java
+# Add javadoc maven plugin
+sed -i "s/<\/project>/<reporting><plugins><plugin><groupId>org.apache.maven.plugins<\/groupId><artifactId>maven-javadoc-plugin<\/artifactId><version>2.9.1<\/version><\/plugin><\/plugins><\/reporting><\/project>/" pom.xml
+mvn clean package javadoc:jar
+
+# Source jar
+pushd src/main/java
+jar -cvf $SCRATCH_DIR/protobuf-java-2.4.1-sources.jar *
+popd
+cp target/protobuf-java-2.4.1-javadoc.jar $SCRATCH_DIR/
+cp target/protobuf-java-2.4.1.jar $SCRATCH_DIR
+cp pom.xml $SCRATCH_DIR/protobuf-java-2.4.1.pom
+
+pushd $SCRATCH_DIR
+gpg --sign --detach-sign -a protobuf-java-2.4.1.jar 
+gpg --sign --detach-sign -a protobuf-java-2.4.1-sources.jar 
+gpg --sign --detach-sign -a protobuf-java-2.4.1-javadoc.jar 
+gpg --sign --detach-sign -a protobuf-java-2.4.1.pom
+bundle_jar=$SCRATCH_DIR/protobuf-java-bundle.jar
+jar -cvf $bundle_jar *pom *asc *jar
+popd
+
+popd
+popd
+
+echo "Created bundle jar at $bundle_jar"
